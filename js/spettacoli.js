af5405cc61c77c7b9b747e2aa8ce8107308ae8ec
tigro7 <alby.tiri@gmail.com>
2019-06-30 16:24:53 +0200

    Aggiunti attori mancanti ad attori.xml Aggiunti link google foto a spettacoli Ruotate alcune foto di iwannadance

--- js/spettacoli.js
+++ js/spettacoli.js
@@ -2,7 +2,7 @@ function loadSpettacoli() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-    parseSpettacoli(this);
+        parseSpettacoli(this);
     }
   };
   xhttp.open("GET", "src/spettacoli.xml", true);
@@ -13,7 +13,7 @@ function parseSpettacoli(xml) {
         var xmlDoc = xml.responseXML;
         var listaSpettacoli = xmlDoc.getElementsByTagName("spettacolo");
         for (i = 0; i < listaSpettacoli.length; i++){
-            console.log("spettacolo" + i.toString);
+            //console.log(listaSpettacoli[i]);
             var cardSpettacolo = '<div class="card spettacoloLight" id="spettacolo' + i.toString() + '">';
             cardSpettacolo +=       '<div class="row">';
             
@@ -25,16 +25,17 @@ function parseSpettacoli(xml) {
             
                 cardSpettacolo +=       '<div class="col-md-8 px-3">';
                 cardSpettacolo +=           '<div class="card-block px-3">';
-                cardSpettacolo +=               '<h4 class="card-title">';
+                cardSpettacolo +=               '<h2 class="card-title">';
                 cardSpettacolo +=                   $(listaSpettacoli[i]).find("titolo").text();
-                cardSpettacolo +=               '</h4>';
+                cardSpettacolo +=               '</h2>';
                 cardSpettacolo +=               '<p class="card-text">';
                 cardSpettacolo +=                   $(listaSpettacoli[i]).find("short").text();
                 cardSpettacolo +=               '</p>';
-                cardSpettacolo +=               '<p class="card-text">';
-                cardSpettacolo +=                   $(listaSpettacoli[i]).find("desc").text();
-                cardSpettacolo +=               '</p>';
-                cardSpettacolo +=               '<a class="btn btn-primary infoBtn">Dettagli</a>';
+                cardSpettacolo +=				'<br>';
+                //cardSpettacolo +=               '<p class="card-text">';
+                //cardSpettacolo +=                   $(listaSpettacoli[i]).find("desc").text();
+                //cardSpettacolo +=               '</p>';
+                cardSpettacolo +=               '<a class="btn btn-primary infoBtn" data-desc="' + $(listaSpettacoli[i]).find("desc").text() + '" data-foto="<foto>' + $(listaSpettacoli[i]).find("foto").html() + '</foto>">Dettagli</a>';
                 cardSpettacolo +=           '</div>';
                 cardSpettacolo +=       '</div>';
             
@@ -52,6 +53,7 @@ function parseSpettacoli(xml) {
             $("#carouselModalSpettacoli").find(".carousel-inner").children().remove();
             $("#spettacolo-modal").find(".modal-body").text("");
             var dir = "index.php?path=" + $(this).parents(".row").find("img").attr("src").replace(".jpg", "") + "/";
+            console.log(dir);
             var fileextension = ".jpg";
             $.ajax({
                 url: dir,
@@ -74,7 +76,19 @@ function parseSpettacoli(xml) {
             $("#spettacolo-modal").find(".modal-title").text($(this).parents(".row").find(".card-title").text());
             var newBody = $("#spettacolo-modal").find(".modal-body").text();
             newBody += $(this).parents(".row").find(".card-text").text();
-            $("#spettacolo-modal").find(".modal-body").text(newBody);
+            newBody += $(this).data("desc");
+            let dataFoto = $(this).data("foto");
+            let parser = new DOMParser();
+            let xmlFoto = parser.parseFromString(dataFoto,"text/xml");
+            let listaFoto = xmlFoto.getElementsByTagName("link");
+            if (listaFoto.length > 0){
+                newBody += '<br>';
+            }
+            for (let f = 0; f < listaFoto.length; f++){
+                let bottoneFoto = '<a class="btn btn-primary fotoBtn" target="_blank" href="' + $(listaFoto[f]).find("url").text() + '">' + $(listaFoto[f]).find("testo").text() + '</a>';
+                newBody += bottoneFoto;
+            }
+            $("#spettacolo-modal").find(".modal-body").html(newBody);
             $("#spettacolo-modal").modal('show');
         })
 }
